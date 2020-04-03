f00fe30d60c150af09e76dd6454429c249841421
iadv.eng <iadv.eng@gmail.com>
2020-04-03 11:49:13 -0400

    Initial commit

---
+++ src/test/java/co/zemoga/portfolio/portfolio/PortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package co.zemoga.portfolio.portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
