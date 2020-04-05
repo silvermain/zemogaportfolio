2de6b0ac38d980af84143499e888b91c81568218
iadv.eng <iadv.eng@gmail.com>
2020-04-04 23:51:42 -0400

    Revert "Rest resources"
    This reverts commit bb945c87acc310cc9e09f8264c5acc803acf554f.

--- src/main/java/co/zemoga/portfolio/portfolio/repository/GameOfThronesRepository.java
+++
@@ -1,7 +0,0 @@
-package co.zemoga.portfolio.portfolio.repository;
-
-import co.zemoga.portfolio.portfolio.model.Actor;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GameOfThronesRepository extends JpaRepository<Actor, Integer> {
-}
--- src/main/java/co/zemoga/portfolio/portfolio/service/GameOfThronesService.java
+++
@@ -1,4 +0,0 @@
-package co.zemoga.portfolio.portfolio.service;
-
-public class GameOfThronesService {
-}
